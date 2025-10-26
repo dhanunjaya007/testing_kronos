9beb1fcfac327e3358d9efd3e75c00ee2a40c529
dhanunjaya007 <dhanunjayachinnu123@gmail.com>
2025-10-26 12:54:35 +0530

    Update celebration.py

--- commands/celebration.py
+++ commands/celebration.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 from datetime import datetime, timedelta
 import logging
-from typing import Optional
+from typing import Optional, Literal
 import random
 
 logger = logging.getLogger(__name__)
